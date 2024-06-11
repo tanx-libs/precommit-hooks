24b10080aec2241b2df3ff4cee1f4292eeaf0c3f
Harsh-fi <harsh.jadon@brine.fi>
2024-06-11 16:22:24 +0530

    update: pre-commit-config

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
-from setuptools import setup, find_packages
+from __future__ import annotations
 
-setup(
-    name='pre-commit-hooks',
-    version='1.0',
-    packages=find_packages(),
-    install_requires=[
-        'pre-commit',
-    ],
-    entry_points={
-        'console_scripts': [
-            'private_key_check = private_key_check:main',
-        ],
-    },
-)
+from setuptools import setup
+setup()
