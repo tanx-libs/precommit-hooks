3fa8b592a7aaa57e8f2ad8a7f72588db0e99f41c
deepaknavaria-tanx <deepak.navaria@brine.fi>
2024-09-10 15:19:23 +0530

    hooks: added failsafe for docker-compose / docker compose

--- pre_commit_hooks/validate_docker_compose.sh
+++ pre_commit_hooks/validate_docker_compose.sh
@@ -5,25 +5,37 @@ compose_files=$(git diff --cached --name-only --diff-filter=ACM | grep -E 'docke
 
 # Check if no docker-compose files are found
 if [[ -z "$compose_files" ]]; then
-    # echo "No docker-compose.yml files found in staged changes."
+    # No docker-compose files found, exit without error
     exit 0
 fi
 
-# check if docker-compose is installed
-if ! command -v docker-compose &> /dev/null
-then
-    echo "docker-compose could not be found"
-    exit 1
+# Initialize the flag for docker-compose presence
+is_docker_compose_present=0
+
+# Check if docker-compose is installed; if not, check for docker
+if ! command -v docker-compose &> /dev/null; then
+    if ! command -v docker &> /dev/null; then
+        echo "docker-compose or docker is not installed. Please install docker-compose or docker to continue."
+        exit 1
+    else
+        is_docker_compose_present=1
+    fi
 fi
 
 # Loop through all staged docker-compose.yml files and validate them
 for file in $compose_files; do
-    # Run Docker Compose config validation
-    docker-compose -f "$file" config --quiet
+    # Run docker-compose if it's installed, otherwise use docker compose
+    if [ $is_docker_compose_present -eq 0 ]; then
+        docker-compose -f "$file" config > /dev/null
+    else
+        docker compose -f "$file" config > /dev/null
+    fi
+    
+    # Check if the command was successful
     if [ $? -ne 0 ]; then
         echo "Docker Compose validation failed for $file"
         exit 1
     fi
 done
 
-# echo "All docker-compose.yml files passed validation"
+# All docker-compose files passed validation
