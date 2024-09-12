51641a2917616e54b7d13c049385969efe86be3c
deepaknavaria-tanx <deepak.navaria@brine.fi>
2024-09-12 15:43:52 +0530

    hooks: fixed yamllint hook

--- pre_commit_hooks/run_yamllint.py
+++ pre_commit_hooks/run_yamllint.py
@@ -17,7 +17,6 @@ def get_staged_yaml_files():
             check=True,
         )
         # Filter for YAML files (both .yaml and .yml) and create absolute paths
-        # yaml_files = [file for file in result.stdout.splitlines() if file.endswith(('.yml', '.yaml'))]
         yaml_files = [
             os.path.abspath(file)
             for file in result.stdout.splitlines()
@@ -30,6 +29,9 @@ def get_staged_yaml_files():
 
 
 def preprocess_yaml_file(yaml_file):
+    """
+    Preprocess the YAML file content by removing lines containing # yamllint disable-line.
+    """
     processed_lines = []
     try:
         with open(yaml_file, "r") as file:
@@ -63,41 +65,30 @@ def check_yamllint_config_exists():
 
 
 def lint_yaml_file(yaml_file, yamllint_path):
-    # Lint the YAML file using yamllint with the provided config
+    """
+    Lint the preprocessed YAML content using yamllint, piping the content via stdin.
+    """
     try:
+        # Preprocess the YAML content by removing lines with # yamllint disable-line
         preprocessed_yaml_content = preprocess_yaml_file(yaml_file)
 
-        temp_yaml_file = yaml_file + ".tmp"
-
-        with open(temp_yaml_file, "w") as temp_file:
-            temp_file.write(preprocessed_yaml_content)
-
+        # Run yamllint and pipe the preprocessed content to stdin
         result = subprocess.run(
-            [
-                yamllint_path,
-                "-c",
-                YAMLLINT_CONFIG_FILE,
-                temp_yaml_file,
-            ],  # Include yaml_file at the end
+            [yamllint_path, "-c", YAMLLINT_CONFIG_FILE, "-"],  # `-` makes yamllint read from stdin
+            input=preprocessed_yaml_content,  # Pass the preprocessed content as input
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
         )
+
         if result.returncode != 0:
             print(f"Yamllint failed on {yaml_file}")
             print(result.stdout)
             sys.exit(1)
-        
 
     except subprocess.CalledProcessError as e:
         print(f"Error running yamllint on {yaml_file}: {e.stderr}")
         sys.exit(1)
-    finally:
-        # Remove the temporary file after linting
-        try:
-            os.remove(temp_yaml_file)
-        except OSError as e:
-            print(f"Error removing temporary file {temp_yaml_file}: {e}")
 
 
 def main():
